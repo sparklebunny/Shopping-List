029b25d87fb2e3b56e57c0b37bdb2255d29d5803
JeriBeth Stevens <jeribeth@gmail.com>
2014-03-08 12:43:01 -0700

    Added more styling and beginning jQuery
    jQuery to add item to list on click

--- js/app.js
+++ js/app.js
@@ -1,4 +1,19 @@
 $(document).ready(function() {
-	$("#main-list ul").append('li');
+	// $("#food").keypress(function(e){
+	// 	if(e.keycode == 13) {
+	// 		$("#add-button").click()
 	
-}
+// 	}
+// }
+		// $(this).(val);
+	
+	$(".plus").on("click", function(){
+		var item = $("#food").val();
+		// var item = $(this).closest('div').find('#food').val();
+		console.log("item", item);
+		var listItem = "<li class='list-item'>"+item+"</li>";
+		$("#main-list").append(listItem);
+
+			});
+		
+});
