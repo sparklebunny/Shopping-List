982c6e81104ba9c262468cb57069dafbe10fb763
JeriBeth Stevens <jeribeth@gmail.com>
2014-03-10 21:43:18 -0600

    Recoded using <form>; added jQuery functionality
    Added jQuery click functioning
    Added checkbox with strikethrough
    Added X to delete item
    
    Next step: restyle CSS

--- js/app.js
+++ js/app.js
@@ -1,19 +1,48 @@
 $(document).ready(function() {
-	// $("#food").keypress(function(e){
-	// 	if(e.keycode == 13) {
-	// 		$("#add-button").click()
 	
-// 	}
-// }
-		// $(this).(val);
-	
-	$(".plus").on("click", function(){
-		var item = $("#food").val();
-		// var item = $(this).closest('div').find('#food').val();
-		console.log("item", item);
-		var listItem = "<li class='list-item'>"+item+"</li>";
-		$("#main-list").append(listItem);
 
-			});
+//MODEL FROM CODECADEMY EXERCISES
+	$("#button").click(function() {
+        var toAdd = $("input[name=checkListItem]").val();
+        $(".list").append('<div class="item"><input type="checkbox" class="checkbox" />' + toAdd + 
+        	'<span class="delete-item">X</span></div>');
+    });
+
+   	$(document).on('click', '.delete-item', function() {
+        $(this).parent().remove();
+
+    });
+
+    $(document).on('click', ".checkbox", function() {
+    	if ($(this).is(':checked')) {
+    		$(this).parent().wrap("<strike>");
+    	}
+
+    	else {
+    		$(this).parent().unwrap("<strike>");
+    	}
+    	
+    	
+    });
+
+    // $(document).on('click', '#item-name', function() {
+    //     $(this).remove();
+    // });
+
+
+
+	// CODE FOR FIRST ATTEMPT WITH UL AND LI
+	// $(".plus").on("click", function(){
+	// 	var item = $("#food").val();
+	// 	// var item = $(this).closest('div').find('#food').val();
+	// 	console.log("item", item);
+	// 	var listItem = "<li class='list-item'>"+item+"</li>";
+	// 	$("#main-list").append(listItem);
+	// 	});
 		
+///////////////////////////////////////////////////////////
+
+
+
 });
+
